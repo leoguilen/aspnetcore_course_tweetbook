1c1c2ddce1d6e8810a98615fa11acbd38858c7cb
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-05-31 21:16:40 -0300

    Add serviço de verificação e autorização do login

---
+++ Controllers/V1/IdentityController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Tweetbook.Contracts.V1;
+using Tweetbook.Contracts.V1.Request;
+using Tweetbook.Contracts.V1.Response;
+using Tweetbook.Services;
+
+namespace Tweetbook.Controllers.V1
+{
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public IdentityController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpPost(ApiRoutes.Identity.Register)]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequest registrationRequest)
+        {
+            var authResponse = await _identityService.RegisterAsync(registrationRequest.Email, registrationRequest.Password);
+
+            if(!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+            return Ok(new AuthSuccessResponse 
+            {
+                Token = authResponse.Token
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Login)]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest loginRequest)
+        {
+            var authResponse = await _identityService.LoginAsync(loginRequest.Email, loginRequest.Password);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResponse.Token
+            });
+        }
+    }
+}
