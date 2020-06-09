5e8fba41e7c6d7f3cd25484b813465ed99288cfa
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-06-08 23:58:20 -0300

    Alteração na estrutura das pastas do projeto

--- Tweetbook.IntegrationTests/obj/Debug/netcoreapp3.1/Tweetbook.IntegrationTests.AssemblyInfo.cs
+++ Tweetbook.IntegrationTests/obj/Debug/netcoreapp3.1/Tweetbook.IntegrationTests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Tweetbook, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "D:\\Git\\Tweetbook\\dotnetcore3.1_tweetbook", "Tweetbook.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Tweetbook, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "D:\\Git\\Nova pasta\\dotnetcore3.1_tweetbook\\Tweetbook", "Tweetbook.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tweetbook.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
