aea15de1fd72d9f0cfee6e0ceb5a6de6b24ab929
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-06-12 22:01:26 -0300

    Add atributo que valida uma ApiKey de exemplo para acessar um endpoint especifico

---
+++ Tweetbook/Controllers/V1/SecretController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Filters;
+
+namespace Tweetbook.Controllers.V1
+{
+    [ApiController]
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secrets");
+        }
+    }
+}
